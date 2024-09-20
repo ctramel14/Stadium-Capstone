1211b6f63331497c42a9aab2ee44abfa5a27ba28
Daniel Vasquez <dwvasquez97@gmail.com>
2024-09-19 21:14:29 -0700

    added CSS styling to stadiums page and changed single page image to inside image

--- stadium-capstone/components/StadiumCards.jsx
+++ stadium-capstone/components/StadiumCards.jsx
@@ -3,6 +3,39 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // import { createStadiums } from "../../prisma/seed.js";
 
+const stadiumColors = {
+  1: "rgb(156,41,59)",
+  2: "rgb(27,57,99)",
+  3: "rgb(233,124,77)",
+  4: "rgb(189,48,57)",
+  5: "rgb(40,67,138)",
+  6: "rgb(0,0,0)",
+  7: "rgb(195,56,55)",
+  8: "rgb(33,53,77)",
+  9: "rgb(65,22,122)",
+  10: "rgb(22,46,81)",
+  11: "rgb(227,131,72)",
+  12: "rgb(41,86,147)",
+  13: "rgb(184,53,57)",
+  14: "rgb(49,105,163)",
+  15: "rgb(80,171,223)",
+  16: "rgb(33,53,88)",
+  17: "rgb(28,59,103)",
+  18: "rgb(236,101,48)",
+  19: "rgb(40,54,87)",
+  20: "rgb(41,130,75)",
+  21: "rgb(216,70,67)",
+  22: "rgb(249,204,92)",
+  23: "rgb(63,54,48)",
+  24: "rgb(240,135,64)",
+  25: "rgb(50,107,108)",
+  26: "rgb(206,65,89)",
+  27: "rgb(36,62,105)",
+  28: "rgb(31,64,131)",
+  29: "rgb(51,90,150)",
+  30: "rgb(172,50,38)",
+};
+
 //fetching all stadiums
 export default function StadiumCards({ stadiums, setStadiums }) {
   const [searchParam, setSearchParam] = useState("");
@@ -38,7 +71,7 @@ export default function StadiumCards({ stadiums, setStadiums }) {
     : stadiums;
 
   return (
-    <>
+    <div className="bg-wrapper">
       <div className="search">
         <label>
           <input
@@ -50,29 +83,33 @@ export default function StadiumCards({ stadiums, setStadiums }) {
           />
         </label>
       </div>
+      <h1>Will You Visit Every Stadium?</h1>
+      <h3>Select 'Explore' to see more information, or 'Visited' to add it to your visited list.</h3>
       <div className="stadiums-grid-container">
-        {stadiumsToDisplay.map((stadium) => {
-          return (
-            <div className="stadium-card" key={stadium.id}>
-              <img
-                src={stadium.imageOutsideURL}
-                alt={`${stadium.name} outside view`}
-              />
-              <br />
-              <strong>
-                <h2>{stadium.name}</h2>
-              </strong>
-              <p>{stadium.teamName}</p>
-              <p>{stadium.state}</p>
+        {stadiumsToDisplay.map((stadium) => (
+          <div
+            className="stadium-card"
+            key={stadium.id}
+            style={{ backgroundColor: stadiumColors[stadium.id] }}
+          >
+            <img
+              src={stadium.imageOutsideURL}
+              alt={`${stadium.name} outside view`}
+            />
+            <strong>
+              <h2>{stadium.name}</h2>
+            </strong>
+            <p>{stadium.teamName}</p>
+            <div className="stadium-card-buttons">
               <button onClick={() => navigate(`/stadiums/${stadium.id}/`)}>
-                Find Out More
+                Explore
               </button>
-              <button>Already Visited</button>
+              <button>Visited</button>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
