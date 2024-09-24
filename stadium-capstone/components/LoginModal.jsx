bd0d13c4e4b373cef0647c1229fed7b8f8cce055
Drake Rogers <drakrgrs@gmail.com>
2024-09-24 07:35:30 -1000

    updated popup

--- stadium-capstone/components/LoginModal.jsx
+++ stadium-capstone/components/LoginModal.jsx
@@ -1,5 +1,5 @@
 import "./LoginModal.css";
-import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const LoginModal = ({
   toggleLogin,
@@ -56,17 +56,11 @@ const LoginModal = ({
           />
           <span>
             <svg
-              // stroke="currentColor"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                //   stroke-width="2"
-                //   stroke-linejoin="round"
-                //   stroke-linecap="round"
-              ></path>
+              <path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
             </svg>
           </span>
         </div>
@@ -81,24 +75,9 @@ const LoginModal = ({
           />
 
           <span>
-            <svg
-              // stroke="currentColor"
-              viewBox="0 0 24 24"
-              // fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                //   stroke-width="2"
-                //   stroke-linejoin="round"
-                //   stroke-linecap="round"
-              ></path>
-              <path
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                //   stroke-width="2"
-                //   stroke-linejoin="round"
-                //   stroke-linecap="round"
-              ></path>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+              <path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
             </svg>
           </span>
         </div>
@@ -108,10 +87,9 @@ const LoginModal = ({
 
         <p className="signup-link">
           No account?
-          <a href="">Sign up</a>
+          <Link to="/users/register">Register</Link>
         </p>
       </form>
-      <button onClick={toggleLogin}>Close</button>
     </>
   );
 };
